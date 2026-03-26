0d8c2edd1842143c7827e0b35c0a716c7dffa358
truxl-admin <admin@truxl.com>
2026-03-26 13:51:15 +0530

    Truxl Added

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 
 const services = [
-  'Mutual Fund Distribution',
+  'Mutual Fund Investment',
   'Tax Planning',
   'Insurance Planning',
   'Retirement Planning',
